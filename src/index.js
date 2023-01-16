30afb451c51cd53ac5ae78306709933eec264c67
Anrii Sereda <102906498+SeredaAndrey@users.noreply.github.com>
2023-01-16 09:14:29 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import { theme } from './components/theme';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename="/goit-react-hw-07-phonebook/">
+      <BrowserRouter basename="/goit-react-hw-08-phonebook/">
         {/* <BrowserRouter> */}
         <Provider store={store}>
           <App />
