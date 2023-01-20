2ebbab723842b2380ac3ad2e9e68130c5baa6073
Andrii Sereda <a.a.sereda@ukr.net>
2023-01-20 15:08:00 +0200

    in progress.......

--- src/components/contactdetails/contactdetails.jsx
+++ src/components/contactdetails/contactdetails.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-// import axios from 'axios';
-import { useState, Suspense } from 'react';
+import { Suspense } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, useNavigate, Outlet } from 'react-router-dom';
 import avatarDefault from './avatarDefault.jpg';
@@ -18,18 +17,12 @@ import {
   RedactButon,
   ButtonContainer,
 } from './contactdetails.styled';
-// import { useEffect } from 'react';
 
 const ContactDetails = () => {
   const dispatch = useDispatch();
   const params = useParams();
   const navigate = useNavigate();
-  // const [Details, setDetails] = useState([]);
   const { items } = useSelector(getContacts);
-
-  // useEffect(() => {
-  //   dispatch(getContactForId());
-  // }, [dispatch, params, items]);
 
   const onDeleteContact = () => {
     dispatch(deleteContact(params.id));
@@ -41,31 +34,19 @@ const ContactDetails = () => {
     navigate('edit');
   };
 
-  const getContactForId = async () => {
-    return items.filter(item => item.id === params.id);
+  const getContactData = () => {
+    return items.filter(item => item.id === params.id)[0];
   };
-
-  //  const fetchContactsById = createAsyncThunk(
-  //   'contacts/fetchAll',
-  //   async thunkAPI => {
-  //     try {
-  //       const responce = await axios.get('/contacts');
-  //       return responce.data;
-  //     } catch (error) {
-  //       return thunkAPI.rejectWithValue(error.message);
-  //     }
-  //   }
-  // );
 
   return (
     <ContactPrimariContainer>
       <ContactDetailsContainer>
         <ContactDetailsName>Contact details</ContactDetailsName>
 
-        <ContactAvatar src={avatarDefault} alt={getContactForId().name} />
-        <ContactDetailsName>name: {getContactForId().name}</ContactDetailsName>
+        <ContactAvatar src={avatarDefault} alt={getContactData().name} />
+        <ContactDetailsName>name: {getContactData().name}</ContactDetailsName>
         <ContactDetailsMore>
-          number: {getContactForId().number}
+          number: {getContactData().number}
         </ContactDetailsMore>
 
         <ButtonContainer>
